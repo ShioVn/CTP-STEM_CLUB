e90015fc064fe70c504f4294c2222ec3344c42de
Shiovn <duongduccuong943@gmail.com>
2026-05-25 23:42:11 +0700

    project: configure prisma to use database env variable for vercel

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,9 @@
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    // Dán trực tiếp chuỗi kết nối Supabase của bạn vào đây
-    url: "postgresql://postgres.mbcajtfwrhaolcjkfsda:j90Id6ysaTuTWImE@aws-1-ap-northeast-2.pooler.supabase.com:6543/postgres?pgbouncer=true",
+    url: env("DATABASE_URL"),
   },
 });
