73f5bf83772cd15d78e451ed823d6e5d4ee459f4
Shiovn <duongduccuong943@gmail.com>
2026-05-25 23:52:31 +0700

    fix: change auth route to force-dynamic

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,3 @@
-export const dynamic = "force-static";
+export const dynamic = "force-dynamic"; // <--- ĐỔI THÀNH DÒNG NÀY
 import { handlers } from "@/lib/auth"
 export const { GET, POST } = handlers
